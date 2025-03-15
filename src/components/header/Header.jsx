28a967e6a379c5ba10ac61711598db281b69f809
Aleksandr Volokhov <volohovaleksandr1974@gmail.com>
2025-03-15 14:02:07 +0300

    header fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,34 +6,37 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import styles from "./Header.module.css";
 
-
 function Header() {
     return (
         <AppBar position="static">
             <Toolbar className={styles.toolbar}>
-                <Typography variant="h6" component="div" className={styles.title}>
-                    <Link to="/" className={styles.link}>
-                        Go Game
-                    </Link>
-                </Typography>
+                <div className={styles.left}>
+                    <Typography variant="h6" component="div" className={styles.title}>
+                        <Link to="/" className={styles.link}>
+                            Go Game
+                        </Link>
+                    </Typography>
 
-                <Button color="inherit">
-                    <Link to="/offline" className={styles.link}>
-                        Offline
-                    </Link>
-                </Button>
+                    <Button color="inherit">
+                        <Link to="/offline" className={styles.link}>
+                            Offline
+                        </Link>
+                    </Button>
+                </div>
 
-                <Button color="inherit">
-                    <Link to="/login" className={styles.link}>
-                        Login
-                    </Link>
-                </Button>
+                <div className={styles.right}>
+                    <Button color="inherit">
+                        <Link to="/login" className={styles.link}>
+                            Login
+                        </Link>
+                    </Button>
 
-                <Button color="inherit">
-                    <Link to="/register" className={styles.link}>
-                        Register
-                    </Link>
-                </Button>
+                    <Button color="inherit">
+                        <Link to="/register" className={styles.link}>
+                            Register
+                        </Link>
+                    </Button>
+                </div>
             </Toolbar>
         </AppBar>
     );
